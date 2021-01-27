b8a81fc8eb86d0ff998d88d1e8519ed7943a820c
Kerem <keremkara30@hotmail.com>
2021-01-27 22:53:41 +0300

    Added attributes, getter setters to some elements

--- Implementation Phase/GELF/src/main/java/gelf/model/elements/Cell.java
+++ Implementation Phase/GELF/src/main/java/gelf/model/elements/Cell.java
@@ -1,5 +1,90 @@
 package main.java.gelf.model.elements;
 
-public class Cell {
+import java.util.ArrayList;
 
+import main.java.gelf.model.elements.attributes.Leakage;
+
+public class Cell extends HigherElement {
+	private float[] index1;
+    private float[] index2;
+    private Library parentLibrary;
+    private ArrayList<InputPin> inPins;
+    private ArrayList<OutputPin> outPins;
+    private Leakage[] leakages;
+    private float defaultLeakage;
+    
+    public Cell(String name, float[] index1, float[] index2, Library parentLibrary,
+    		ArrayList<InputPin> inPins, ArrayList<OutputPin> outPins,
+    		Leakage[] leakages, float leakage) {
+    	super.setName(name);
+    	this.index1 = index1;
+    	this.index2 = index2;
+    	this.parentLibrary = parentLibrary;
+    	this.inPins = inPins;
+    	this.outPins = outPins;
+    	this.leakages = leakages;
+    	this.defaultLeakage = leakage;
+    }
+    
+	@Override
+	public void calculate() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public float[] getIndex1() {
+		return index1;
+	}
+
+	public void setIndex1(float[] index1) {
+		this.index1 = index1;
+	}
+
+	public float[] getIndex2() {
+		return index2;
+	}
+
+	public void setIndex2(float[] index2) {
+		this.index2 = index2;
+	}
+
+	public Library getParentLibrary() {
+		return parentLibrary;
+	}
+
+	public void setParentLibrary(Library parentLibrary) {
+		this.parentLibrary = parentLibrary;
+	}
+
+	public ArrayList<InputPin> getInPins() {
+		return inPins;
+	}
+
+	public void setInPins(ArrayList<InputPin> inPins) {
+		this.inPins = inPins;
+	}
+
+	public ArrayList<OutputPin> getOutPins() {
+		return outPins;
+	}
+
+	public void setOutPins(ArrayList<OutputPin> outPins) {
+		this.outPins = outPins;
+	}
+
+	public Leakage[] getLeakages() {
+		return leakages;
+	}
+
+	public void setLeakages(Leakage[] leakages) {
+		this.leakages = leakages;
+	}
+
+	public float getDefaultLeakage() {
+		return defaultLeakage;
+	}
+
+	public void setDefaultLeakage(float defaultLeakage) {
+		this.defaultLeakage = defaultLeakage;
+	}
 }
--- Implementation Phase/GELF/src/main/java/gelf/model/elements/Library.java
+++ Implementation Phase/GELF/src/main/java/gelf/model/elements/Library.java
@@ -1,5 +1,75 @@
 package main.java.gelf.model.elements;
 
-public class Library {
+import java.util.ArrayList;
 
+public class Library extends HigherElement {
+	private float[] index1;
+    private float[] index2;
+    private String path;
+    private String[] fileData;
+    private ArrayList<Cell> cells;
+    private Stat defaultLeakage;
+    
+    public Library(String name, float[] index1, float[] index2, 
+    		String path, ArrayList<Cell> cells) {
+    	super.setName(name);
+    	this.index1 = index1;
+    	this.index2 = index2;
+    	this.path = path;
+    	this.cells = cells;
+    }
+    
+	public float[] getIndex1() {
+		return index1;
+	}
+	
+	public void setIndex1(float[] index1) {
+		this.index1 = index1;
+	}
+	
+	public float[] getIndex2() {
+		return index2;
+	}
+	
+	public void setIndex2(float[] index2) {
+		this.index2 = index2;
+	}
+	
+	public String getPath() {
+		return path;
+	}
+	
+	public void setPath(String path) {
+		this.path = path;
+	}
+	
+	public String[] getFileData() {
+		return fileData;
+	}
+	
+	public void setFileData(String[] fileData) {
+		this.fileData = fileData;
+	}
+	
+	public ArrayList<Cell> getCells() {
+		return cells;
+	}
+	
+	public void setCells(ArrayList<Cell> cells) {
+		this.cells = cells;
+	}
+	
+	public Stat getDefaultLeakage() {
+		return defaultLeakage;
+	}
+	
+	public void setDefaultLeakage(Stat defaultLeakage) {
+		this.defaultLeakage = defaultLeakage;
+	}
+
+	@Override
+	public void calculate() {
+		// TODO Auto-generated method stub
+		
+	}
 }
