ff598b0d8f4bf1f853008466d8be217023086090
AlpTorac <a.torac@hotmail.com>
2021-01-29 16:47:20 +0100

    Implemented Position classes

--- Implementation Phase/GELF/src/main/java/gelf/view/diagrams/components/PositionIn2DDiagram.java
+++ Implementation Phase/GELF/src/main/java/gelf/view/diagrams/components/PositionIn2DDiagram.java
@@ -1,20 +1,54 @@
 package gelf.view.diagrams.components;
 
 public class PositionIn2DDiagram extends PositionInDiagram {
+	
+	private static final Number[][] TO_2D_MATRIX = {{1, 0}, {0, 1}};
+	
 	public PositionIn2DDiagram(DiagramAxis xAxis, Number xCoordinate, DiagramAxis yAxis, Number yCoordinate) {
 		super(new DiagramAxis[] {xAxis, yAxis},
 				new Number[] {xCoordinate, yCoordinate});
 	}
 	public void setXCoordinate(Number xCoordinate) {
-		
+		this.setAxisCoordinate(0, xCoordinate);
 	}
 	public Number getXCoordinate() {
-		return 0;
+		return this.getAxisPos(0);
 	}
 	public void setYCoordinate(Number yCoordinate) {
-		
+		this.setAxisCoordinate(1, yCoordinate);
 	}
 	public Number getYCoordinate() {
-		return 0;
+		return this.getAxisPos(1);
+	}
+	/**
+	 * Converts each axis coordinate to 2D coordinates (x, y) using {@link #TO_2D_MATRIX}.
+	 * 
+	 * @return The converted coordinates
+	 */
+	@Override
+	protected Number[] to2D() {
+		double xCoordinate = this.to2DCoordinate(0).doubleValue();
+		double yCoordinate = this.to2DCoordinate(1).doubleValue();
+		
+		return new Number[] {xCoordinate, yCoordinate};
+	}
+	
+	/**
+	 * Converts each axis coordinate to one of the 2D coordinates (x, y) using {@link #TO_2D_MATRIX}.
+	 * 
+	 * @return The converted coordinates
+	 */
+	@Override
+	protected Number to2DCoordinate(int index) {
+		double coordinate = 0;
+		
+		Number[][] tMatrix = TO_2D_MATRIX;
+		
+		for (int i = 0; i < tMatrix.length; i++) {
+			coordinate += tMatrix[i][0].doubleValue() * this.getXCoordinate().doubleValue();
+			coordinate += tMatrix[i][1].doubleValue() * this.getYCoordinate().doubleValue();
+		}
+		
+		return coordinate;
 	}
 }
--- Implementation Phase/GELF/src/main/java/gelf/view/diagrams/components/PositionInDiagram.java
+++ Implementation Phase/GELF/src/main/java/gelf/view/diagrams/components/PositionInDiagram.java
@@ -5,21 +5,40 @@ public abstract class PositionInDiagram {
 	private Number[] positionsInAxes;
 	
 	public PositionInDiagram(DiagramAxis[] axes, Number[] coordinatesInAxes) {
-		
+		this.axes = axes;
+		this.positionsInAxes = coordinatesInAxes;
 	}
 	public Number axisCoordinateToFrameCoordinate(int index) {
-		return 0;
+		return to2DCoordinate(index);
 	}
 	public PositionInFrame toPositionInFrame() {
-		return null;
+		Number[] coordinates = to2D();
+		return new PositionInFrame(coordinates[0], coordinates[1]);
 	}
 	protected void setAxisCoordinate(int index, Number position) {
-		
+		this.positionsInAxes[index] = position;
 	}
 	protected void setAxisCoordinates(Number[] coordinates) {
-		
+		this.positionsInAxes = coordinates;
 	}
 	protected Number getAxisPos(int index) {
-		return 0;
+		return this.positionsInAxes[index];
 	}
+	/**
+	 * Converts each axis coordinate to 2D coordinates (x, y) using the transformation matrix
+	 * inside the subclass.
+	 * 
+	 * @return The converted coordinates
+	 */
+	protected abstract Number[] to2D();
+	
+	/**
+	 * Converts each axis coordinate to one of the 2D coordinates (x, y) using the transformation matrix
+	 * inside the subclass.
+	 * 
+	 * @param index The row of the said transformation matrix to use.
+	 * 
+	 * @return The converted coordinates
+	 */
+	protected abstract Number to2DCoordinate(int index);
 }
