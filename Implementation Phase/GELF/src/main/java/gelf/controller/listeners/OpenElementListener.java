4e2f4b6613fae2458592426488b7606c799634e9
egeuzhan <ege.uzhan1999@gmail.com>
2021-02-14 18:00:51 +0100

    Fix exception thrown after expanding and collapsing

--- Implementation Phase/GELF/src/main/java/gelf/controller/listeners/OpenElementListener.java
+++ Implementation Phase/GELF/src/main/java/gelf/controller/listeners/OpenElementListener.java
@@ -16,6 +16,7 @@ import javax.swing.tree.DefaultMutableTreeNode;
 import javax.swing.tree.TreePath;
 
 import gelf.model.elements.Element;
+import gelf.model.project.Model;
 
 /*
  * Listener for opening an element in the working area.
@@ -37,17 +38,21 @@ public class OpenElementListener implements ActionListener, MouseListener {
 			return;
 		}
 		Element element = outliner.getSelectedElements().get(0);
-		subwindows.addSubWindow(new SubWindow(element, subwindows, 100, 100));
+		subwindows.addSubWindow(new SubWindow(element, Model.getInstance().getCurrentProject() ,subwindows, 100, 100));
 		
 	}
 
 
 	@Override
 	public void mouseClicked(MouseEvent e) {
-		if (e.getClickCount() == 2 && (e.getSource()) instanceof DefaultMutableTreeNode) {
-			TreePath path = outliner.tree.getPathForLocation(e.getX(), e.getY());
-			Element element = (Element) ((DefaultMutableTreeNode)path.getLastPathComponent()).getUserObject();
-			subwindows.addSubWindow(new SubWindow(element, subwindows, 100, 100));			
+		if (e.getClickCount() == 2) {
+			DefaultMutableTreeNode node = (DefaultMutableTreeNode)
+                    outliner.tree.getLastSelectedPathComponent();
+             if (node == null) {
+            	 return;
+             }
+			Element element = (Element) node.getUserObject();
+			subwindows.addSubWindow(new SubWindow(element, Model.getInstance().getCurrentProject(), subwindows, 100, 100));			
 		}
 		
 	}
