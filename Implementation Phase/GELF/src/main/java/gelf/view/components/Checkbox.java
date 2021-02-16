2f28c74231b5a868ae18b9a1567afbc742f55db1
Manuel Schenk <schenkman30.06@gmail.com>
2021-02-16 14:18:54 +0100

    Implement statistis bar and checkboxes

--- Implementation Phase/GELF/src/main/java/gelf/view/components/Checkbox.java
+++ Implementation Phase/GELF/src/main/java/gelf/view/components/Checkbox.java
@@ -2,10 +2,15 @@ package gelf.view.components;
 
 import javax.swing.JCheckBox;
 
+import gelf.view.composites.ColorTheme;
+
 /**
  * Checkbox
  */
 public class Checkbox extends JCheckBox {
-
-    
+    public Checkbox(String name) {
+        super(name);
+        this.setBackground(ColorTheme.interactable);
+        this.setForeground(ColorTheme.text);
+    }
 }
--- Implementation Phase/GELF/src/main/java/gelf/view/composites/Visualizer.java
+++ Implementation Phase/GELF/src/main/java/gelf/view/composites/Visualizer.java
@@ -7,8 +7,10 @@ import gelf.model.project.Project;
 
 import java.awt.*;
 
+import javax.swing.Box;
 import javax.swing.BoxLayout;
 
+import gelf.view.components.Checkbox;
 import gelf.view.components.Panel;
 import gelf.view.representation.CellPanel;
 import gelf.view.representation.DataPanel;
@@ -21,13 +23,14 @@ public class Visualizer extends ElementManipulator {
 	SubWindow subWindow;
 	DataPanel dataPanel;
 	Panel upperPanel;
+	Panel lowerPanel;
 	
     public Visualizer(gelf.model.elements.Element e, SubWindow w, Project p, int width, int height) {
         super(e, p, width, height);
         this.subWindow = w;
-        this.setBackground(Color.green);
+		//style
         this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
-        
+        //cell display
         upperPanel = new Panel(width, height);
         upperPanel.setLayout(new FlowLayout());
         dataPanel = new DataPanel(100, 100, e);
@@ -41,7 +44,32 @@ public class Visualizer extends ElementManipulator {
 	    }
         upperPanel.add(dataPanel);
         dataPanel.setAlignmentY(CENTER_ALIGNMENT);
-        
+        //diagram display
+		this.lowerPanel = new Panel(width, height);
+		this.lowerPanel.setLayout(new BorderLayout());
+		this.lowerPanel.setBackground(ColorTheme.subsection);
+		this.lowerPanel.setVisible(true);
+		//statistics checkboxes
+		Panel stats = new Panel(width, height);
+		stats.setLayout(new BoxLayout(stats, BoxLayout.X_AXIS));
+		stats.setBackground(ColorTheme.section);
+		stats.setVisible(true);
+
+		Checkbox min = new Checkbox("Minimum");
+		min.setVisible(true);
+		stats.add(min);
+		Checkbox max = new Checkbox("Maximum");
+		max.setVisible(true);
+		stats.add(max);
+		Checkbox avg = new Checkbox("Average");
+		avg.setVisible(true);
+		stats.add(avg);
+		Checkbox med = new Checkbox("Median");
+		med.setVisible(true);
+		stats.add(med);
+		//stats.add(Box.createHorizontalGlue());
+		this.lowerPanel.add(stats, BorderLayout.PAGE_START);
+		this.add(this.lowerPanel);
     } 
     
     public void setElement(Element e) {
