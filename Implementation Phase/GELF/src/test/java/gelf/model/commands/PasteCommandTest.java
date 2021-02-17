1d206c36b07871a48f1b897a361791af0fa322bc
Kerem <keremkara30@hotmail.com>
2021-02-17 15:13:35 +0100

    Implement undo for move and paste and test them

--- Implementation Phase/GELF/src/test/java/gelf/model/commands/PasteCommandTest.java
+++ Implementation Phase/GELF/src/test/java/gelf/model/commands/PasteCommandTest.java
@@ -47,5 +47,34 @@ class PasteCommandTest {
 		Assertions.assertEquals(destinationLibrary.getCells().get(3).getName(), 
 				"cell2");
 	}
+	
+	@Test
+	void undoTest() {
+		Library destinationLibrary = null;
+		Library initialLibrary = null;
+		ArrayList<InputPin> inPinList = new ArrayList<InputPin>();
+		ArrayList<OutputPin> outPinList = new ArrayList<OutputPin>();
+		float[] values = {1f, 2f};
+		Leakage leakage = new Leakage(values);
+		Cell cell1 = new Cell("cell1", null, null, initialLibrary, inPinList, outPinList, leakage, 0);
+		Cell cell2 = new Cell("cell2", null, null, initialLibrary, inPinList, outPinList, leakage, 0);
+		Cell cell3 = new Cell("cell3", null, null, initialLibrary, inPinList, outPinList, leakage, 0);
+		Cell cell4 = new Cell("cell4", null, null, initialLibrary, inPinList, outPinList, leakage, 0);
+		ArrayList<Cell> cellList1 = new ArrayList<Cell>();
+		ArrayList<Cell> cellList2 = new ArrayList<Cell>();
+		cellList1.add(cell1);
+		cellList1.add(cell2);
+		cellList2.add(cell3);
+		cellList2.add(cell4);
+		initialLibrary = new Library("library1", null, null, null, cellList1);
+		destinationLibrary = new Library("library2", null, null, null, cellList2);
+		
+		PasteCommand paste = new PasteCommand(destinationLibrary, cellList1);
+		paste.execute();
+		paste.undo();
+		
+		Assertions.assertEquals(2, initialLibrary.getCells().size());
+		Assertions.assertEquals(2, destinationLibrary.getCells().size());
+	}
 
 }
