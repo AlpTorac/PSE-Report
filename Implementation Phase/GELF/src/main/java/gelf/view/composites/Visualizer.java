abf8f5ba3bc2d4dc0bf0387f689cf329c0ef2d92
Manuel Schenk <schenkman30.06@gmail.com>
2021-02-17 11:08:35 +0100

    Add changes from meeting

--- Implementation Phase/GELF/src/main/java/gelf/view/composites/Visualizer.java
+++ Implementation Phase/GELF/src/main/java/gelf/view/composites/Visualizer.java
@@ -4,6 +4,7 @@ import gelf.model.elements.Cell;
 import gelf.model.elements.Element;
 import gelf.model.elements.Library;
 import gelf.model.elements.Stat;
+import gelf.model.elements.attributes.Timing;
 import gelf.model.project.Project;
 
 import java.awt.*;
@@ -14,6 +15,7 @@ import javax.swing.Box;
 import javax.swing.BoxLayout;
 
 import gelf.view.components.Checkbox;
+import gelf.view.components.DropdownSelector;
 import gelf.view.components.Panel;
 import gelf.view.diagrams.DiagramWizard;
 import gelf.view.diagrams.IDiagram;
@@ -31,7 +33,83 @@ public class Visualizer extends ElementManipulator {
 	Panel lowerPanel;
 	Panel diagramPanel;
 	IDiagram diagram;
-	
+	// dropdown enums
+	//attributes
+    public enum Attribute {
+        INPUT_INTERNAL_POWER("Input Internal Power"),
+        OUTPUT_INTERNAL_POWER("Output Internal Power"),
+		LEAKAGE_POWER("Leakage Power"),
+		OUTPUT_TIMING("Output Timing");
+        private String str;
+        private Attribute(String s) {
+            this.str = s;
+        }
+
+        @Override
+        public String toString() {
+            return this.str;
+        }
+    }
+	//internal power
+		//power groups
+		public enum PowerGroups {
+			FALL_POWER("Fall-Power"),
+			RISE_POWER("Rise-Power");
+			private String str;
+			private PowerGroups(String s) {
+				this.str = s;
+			}
+
+			@Override
+			public String toString() {
+				return this.str;
+			}
+		}
+	//output timing
+		//timing sense
+		public enum TimingSense {
+			POSITIVE_UNNATE("Positive-Unate"),
+			NEGATIVE_UNNATE("Negative-Unate"),
+			NON_UNNATE("Non-Unate");
+			private String str;
+			private TimingSense(String s) {
+				this.str = s;
+			}
+
+			@Override
+			public String toString() {
+				return this.str;
+			}
+		}
+		public enum TimingType {
+			COMBINATIONAL("Combinational"),
+			THREE_STATE_ENABLE("Three-State-Enable");
+			private String str;
+			private TimingType(String s) {
+				this.str = s;
+			}
+
+			@Override
+			public String toString() {
+				return this.str;
+			}
+		}
+		public enum TimingGroup {
+			CELL_FALL("Cell-Fall"),
+			CELL_RISE("Cell-Rise"),
+			FALL_TRANSITION("Fall-Transition"),
+			RISE_TRANSITION("Rise-Transition");
+			private String str;
+			private TimingGroup(String s) {
+				this.str = s;
+			}
+
+			@Override
+			public String toString() {
+				return this.str;
+			}
+		}
+
     public Visualizer(gelf.model.elements.Element e, SubWindow w, Project p, int width, int height) {
         super(e, p, width, height);
         this.subWindow = w;
@@ -51,11 +129,17 @@ public class Visualizer extends ElementManipulator {
 	    }
         upperPanel.add(dataPanel);
         dataPanel.setAlignmentY(CENTER_ALIGNMENT);
-        //diagram display
+        //diagram display panel
 		this.lowerPanel = new Panel(width, height);
 		this.lowerPanel.setLayout(new BorderLayout());
 		this.lowerPanel.setBackground(ColorTheme.subsection);
 		this.lowerPanel.setVisible(true);
+		//dropdowns
+		Panel dropdowns = new Panel(width, height);
+		dropdowns.setLayout(new BoxLayout(dropdowns, BoxLayout.X_AXIS));
+		dropdowns.setBackground(ColorTheme.section);
+		dropdowns.setVisible(true);
+
 		//statistics checkboxes
 		Panel stats = new Panel(width, height);
 		stats.setLayout(new BoxLayout(stats, BoxLayout.X_AXIS));
@@ -77,8 +161,12 @@ public class Visualizer extends ElementManipulator {
 		this.lowerPanel.add(stats, BorderLayout.PAGE_END);
 		//diagram viewport
 		DiagramWizard wiz = new DiagramWizard();
+
+		Cell cell = (Cell)e;
+		float[] leakageValues = cell.getLeakages().getValues();
 		ArrayList<float[]> data = new ArrayList<float[]>();
-		data.add(new float[]{1,2,3.5f,5.3f,4.9f});
+		data.add(leakageValues);
+		
 		this.diagramPanel = new Panel(500, 500);
 		this.diagramPanel.setBackground(ColorTheme.text);
 		this.diagramPanel.setLayout(null);
@@ -112,10 +200,8 @@ public class Visualizer extends ElementManipulator {
 		super.componentResized(e);
 		
 		DiagramWizard wiz = new DiagramWizard();
-		ArrayList<float[]> data = new ArrayList<float[]>();
-		data.add(new float[]{1,2,3.5f,5.3f,4.9f});
 		this.diagram.removeFromContainer();
-		this.diagram = wiz.makeBarChart(this.diagramPanel, data);
+		this.diagram = wiz.makeBarChart(this.diagramPanel, this.diagram.cloneData());
 		this.diagram.attachToContainer(this.diagramPanel);
 		this.diagram.refresh();
 	}
