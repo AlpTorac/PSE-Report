46d263d886275b058af80532651377ab368e7326
AlpTorac <a.torac@hotmail.com>
2021-02-18 10:19:51 +0100

    Add DiagramWizard to BarChartBuilderTest
    Now there are 2 test cases that use DiagramWizard

--- Implementation Phase/GELF/src/test/java/gelf/view/diagrams/builder/BarChartBuilderTest.java
+++ Implementation Phase/GELF/src/test/java/gelf/view/diagrams/builder/BarChartBuilderTest.java
@@ -7,7 +7,9 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import gelf.view.diagrams.DiagramWizard;
 import gelf.view.diagrams.IDiagram;
+import gelf.view.diagrams.IDiagramWizard;
 import gelf.view.diagrams.SettingsProvider;
 import gelf.view.diagrams.TestCase;
 import gelf.view.diagrams.TestFrame;
@@ -70,6 +72,91 @@ class BarChartBuilderTest implements TestCase {
 		}
 		
 		diagram.refresh();
-		show(frame, TestCase.LONG_SHOW_DURATION);
+		show(frame, TestCase.SHOW_DURATION);
+		diagram.hide();
+		diagram.removeFromContainer();
+		container.repaint();
+	}
+	
+	@Test
+	void buildWithWizardTest() {
+		data = new ArrayList<float[]>();
+		float[] values = new float[] {6, 5, 1, 1, 2, 3};
+		data.add(values);
+		
+		desc = new ArrayList<String[]>();
+		String[] descs = new String[] {"cell1", "cell2", "cell3", "cell4", "cell5", "cell6"};
+		desc.add(descs);
+		IDiagramWizard wiz = new DiagramWizard();
+		IDiagram diagram = wiz.makeAndAttachBarChart(container, data);
+		
+		DiagramValueDisplayComponent[] bars = diagram.getDiagramValueDisplayComponentPrototypes();
+		int barCount = bars.length;
+		
+		Assertions.assertEquals(data.get(0).length, barCount);
+		Assertions.assertNull(diagram.getNonValueDisplayDiagramComponentPrototype());
+		
+		double halfBarWidthInSteps = SettingsProvider.getInstance().getBarChartBarWidthInSteps() / 2d;
+		
+		for (int i = 1; i <= barCount; i++) {
+			BarChartBar bar = (BarChartBar) bars[i - 1];
+			
+			Assertions.assertEquals(values[i - 1], bar.getValue());
+			
+			PositionIn2DDiagram topLeft = bar.getTopLeftInDiagram();
+			PositionIn2DDiagram bottomRight = bar.getBottomRightInDiagram();
+			
+			Assertions.assertEquals(values[i - 1], topLeft.getYCoordinate());
+			Assertions.assertEquals(i - halfBarWidthInSteps, topLeft.getXCoordinate());
+			Assertions.assertEquals(0, bottomRight.getYCoordinate());
+			Assertions.assertEquals(i + halfBarWidthInSteps, bottomRight.getXCoordinate());
+		}
+		
+		diagram.refresh();
+		show(frame, TestCase.SHOW_DURATION);
+		diagram.hide();
+		diagram.removeFromContainer();
+		container.repaint();
+	}
+	
+	@Test
+	void buildWithWizardAndDescTest() {
+		data = new ArrayList<float[]>();
+		float[] values = new float[] {6, 5, 1, 1, 2, 3};
+		data.add(values);
+		
+		desc = new ArrayList<String[]>();
+		String[] descs = new String[] {"cell1", "cell2", "cell3", "cell4", "cell5", "cell6"};
+		desc.add(descs);
+		IDiagramWizard wiz = new DiagramWizard();
+		IDiagram diagram = wiz.makeAndAttachBarChartWithDescriptions(container, data, desc);
+		
+		DiagramValueDisplayComponent[] bars = diagram.getDiagramValueDisplayComponentPrototypes();
+		int barCount = bars.length;
+		
+		Assertions.assertEquals(data.get(0).length, barCount);
+		Assertions.assertNull(diagram.getNonValueDisplayDiagramComponentPrototype());
+		
+		double halfBarWidthInSteps = SettingsProvider.getInstance().getBarChartBarWidthInSteps() / 2d;
+		
+		for (int i = 1; i <= barCount; i++) {
+			BarChartBar bar = (BarChartBar) bars[i - 1];
+			
+			Assertions.assertEquals(values[i - 1], bar.getValue());
+			
+			PositionIn2DDiagram topLeft = bar.getTopLeftInDiagram();
+			PositionIn2DDiagram bottomRight = bar.getBottomRightInDiagram();
+			
+			Assertions.assertEquals(values[i - 1], topLeft.getYCoordinate());
+			Assertions.assertEquals(i - halfBarWidthInSteps, topLeft.getXCoordinate());
+			Assertions.assertEquals(0, bottomRight.getYCoordinate());
+			Assertions.assertEquals(i + halfBarWidthInSteps, bottomRight.getXCoordinate());
+		}
+		
+		diagram.refresh();
+		show(frame, TestCase.SHOW_DURATION);
+		diagram.hide();
+		diagram.removeFromContainer();
+		container.repaint();
 	}
 }
