62940324fe9b6a0143525e48005f1b40d94dd603
Ege Uzhan <75040006+egeuzhan@users.noreply.github.com>
2021-02-18 21:48:30 +0100

    Delete line printing true/false

--- Implementation Phase/GELF/src/main/java/gelf/view/representation/CellPanel.java
+++ Implementation Phase/GELF/src/main/java/gelf/view/representation/CellPanel.java
@@ -108,7 +108,6 @@ public class CellPanel extends Panel implements MouseListener, ItemListener{
 		selectedPins = new ArrayList<InputPin>();
 		outputPins = cell.getOutPins();
 		maxPins = (inputPins.size() < outputPins.size()) ? outputPins.size() : inputPins.size();
-		System.out.println(cell.getParentLibrary()==null);
 		libButton = new Label(cell.getParentLibrary().getName());
 		cellButton = new Label(cell.getName());
 		
