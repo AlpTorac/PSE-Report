60dc60995a5454e0ac909a9d9a75b11cc3f36b83
Ege Uzhan <75040006+egeuzhan@users.noreply.github.com>
2021-02-18 12:27:41 +0100

    Add constructor to elementmanipulator for comparer

--- Implementation Phase/GELF/src/main/java/gelf/view/composites/ElementManipulator.java
+++ Implementation Phase/GELF/src/main/java/gelf/view/composites/ElementManipulator.java
@@ -1,10 +1,10 @@
 package gelf.view.composites;
 
-import javax.lang.model.element.Element;
 import gelf.model.elements.*;
 import gelf.model.project.Project;
 
 import java.awt.*;
+import java.util.ArrayList;
 
 import gelf.view.components.Panel;
 
@@ -14,12 +14,19 @@ import gelf.view.components.Panel;
 public abstract class ElementManipulator extends Panel {
     gelf.model.elements.Element element;
     Project project;
+    ArrayList<Element> elements;
 
     public ElementManipulator(gelf.model.elements.Element e, Project p, int width, int height) {
         super(width, height);
         this.element = e;
         this.project = p;
         this.setBackground(ColorTheme.section);
+    }
+    
+    public ElementManipulator(ArrayList<Element> elements, Project p, int width, int height) {
+    	super(width, height);
+    	this.elements = elements;
+    	this.project = p;
     }
 
     public gelf.model.elements.Element getElement() {
