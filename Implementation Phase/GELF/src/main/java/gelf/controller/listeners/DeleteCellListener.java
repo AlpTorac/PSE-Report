984c8ae79ab0ff2843e8b80b811ab3cd97617921
Ege Uzhan <75040006+egeuzhan@users.noreply.github.com>
2021-03-16 14:39:54 +0100

    Update DeleteCellListener.java

--- Implementation Phase/GELF/src/main/java/gelf/controller/listeners/DeleteCellListener.java
+++ Implementation Phase/GELF/src/main/java/gelf/controller/listeners/DeleteCellListener.java
@@ -55,16 +55,14 @@ public class DeleteCellListener implements ActionListener {
 		DeleteCommand delete = new DeleteCommand(cells);
 		delete.execute();
 		ArrayList<SubWindow> toRemove = new ArrayList<SubWindow>();
-		for (SubWindow str : subwindows.subWindows) {
-		    if (cells.contains(str.getElement())) {
-		        toRemove.add(str);
+		for (SubWindow window : subwindows.subWindows) {
+		    if (cells.contains(window.getElement())) {
+		        toRemove.add(window);
 		    }
-		 /*   else if () {
-		    	
+		    else if (window.getElement() instanceof Pin && cells.contains(((Pin) window.getElement()).getParent())) {
+		    	toRemove.add(window);
 		    }
-		    else if () {
-		    	
-		    }*/
+		    
 		}
 		for (SubWindow str: toRemove) {
 			subwindows.removeSubWindow(str);
