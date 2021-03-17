2cfdab5487896718ae63a01efe51053fa4c5a4b7
Kerem <keremkara30@hotmail.com>
2021-03-18 00:58:53 +0100

    Implement method for histogram comparison with diferent indexes

--- Implementation Phase/GELF/src/main/java/gelf/model/elements/attributes/InputPower.java
+++ Implementation Phase/GELF/src/main/java/gelf/model/elements/attributes/InputPower.java
@@ -1,4 +1,9 @@
 package gelf.model.elements.attributes;
+
+import java.util.ArrayList;
+
+import gelf.model.project.Interpolator;
+
 /**
  * Keeps data of input internal power.
  * @author Kerem Kara
@@ -25,5 +30,18 @@ public class InputPower extends InAttribute {
 
 	public void setPowGroup(PowerGroup powGroup) {
 		this.powGroup = powGroup;
+	}
+	/**
+	 * 
+	 * @param secIndex Index of the attribute to compare 
+	 * @param secValues Values of the attribute to compare
+	 * @return First attributes values for the second attributes index as [0], second attributes values for first attributes index as [1]
+	 */
+	public float[][] createComparedAttribute(float[] secIndex, float[] secValues) {
+		Interpolator interpolator = new Interpolator();
+		float[][] data = new float[2][];
+		data[0] = interpolator.interpolate(this.index1, this.values, secIndex);
+		data[1] = interpolator.interpolate(secIndex, secValues, this.index1);
+		return data;
 	}
 }
