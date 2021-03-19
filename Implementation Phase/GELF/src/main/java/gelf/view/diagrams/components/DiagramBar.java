3cea20dc3efb33ab184005728972ce56f821b6f3
AlpTorac <a.torac@hotmail.com>
2021-03-19 21:30:36 +0100

    Improve getRoundedPositionInDiagramString

--- Implementation Phase/GELF/src/main/java/gelf/view/diagrams/components/DiagramBar.java
+++ Implementation Phase/GELF/src/main/java/gelf/view/diagrams/components/DiagramBar.java
@@ -3,6 +3,7 @@ package gelf.view.diagrams.components;
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Rectangle;
+import java.util.ArrayList;
 
 import javax.swing.BorderFactory;
 import javax.swing.JLabel;
@@ -100,11 +101,12 @@ public abstract class DiagramBar extends DiagramValueDisplayComponent {
 
 	@Override
 	protected String getRoundedPositionInDiagramString() {
-		String result = "";
-		
-		result += "index1: " +
-		String.valueOf(this.getRoundedString(this.getTopLeftInDiagram().getXCoordinate())) + " - " +
-		String.valueOf(this.getRoundedString(this.getBottomRightInDiagram().getXCoordinate()));
+		int indexPosition = this.diagram.getIndexPositionsOfComponent(this)[0];
+		float[] indices = this.diagram.cloneDiagramData().extractIndices().get(0);
+		float beginIndex = (indexPosition - 1 >= 0) ? indices[indexPosition - 1] : 0;
+		String result = "index1: " +
+		String.valueOf(beginIndex) + " - " +
+		String.valueOf(indices[indexPosition]);
 		
 		return result;
 	}
