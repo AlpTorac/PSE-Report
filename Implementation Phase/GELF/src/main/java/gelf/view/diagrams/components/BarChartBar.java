f8e720f759ad81d9200a4020748af85540bf4d1d
AlpTorac <a.torac@hotmail.com>
2021-03-20 19:29:09 +0100

    Fix and add commentary to BarChartBar
    Fixed an issue, where the description for a value would
    not be displayed for bar chart bars, when they were not
    the default descriptions (instead, the default
    descriptions would be displayed).

--- Implementation Phase/GELF/src/main/java/gelf/view/diagrams/components/BarChartBar.java
+++ Implementation Phase/GELF/src/main/java/gelf/view/diagrams/components/BarChartBar.java
@@ -1,9 +1,13 @@
 package gelf.view.diagrams.components;
 
 import java.awt.Color;
+import java.util.ArrayList;
 
+/**
+ * The class that represents a bar of a {@link gelf.view.diagrams.type.BarChart BarChart}.
+ * @author Alp Torac Genc
+ */
 public class BarChartBar extends DiagramBar {
-
 	protected BarChartBar(Color color, float value, PositionIn2DDiagram topLeft, PositionIn2DDiagram bottomRight,
 			int borderThickness) {
 		super(color, value, topLeft, bottomRight, borderThickness);
@@ -20,7 +24,11 @@ public class BarChartBar extends DiagramBar {
 	
 	@Override
 	protected String getRoundedPositionInDiagramString() {
-		String result = "bar " + this.diagram.getIndexPositionsOfComponent(this)[0];
+		ArrayList<String[]> descs = this.diagram.cloneDiagramData().extractValueDescriptions();
+		String description = (descs != null) ? 
+				descs.get(0)[this.diagram.getIndexPositionsOfComponent(this)[0]]:
+				String.valueOf(this.diagram.getIndexPositionsOfComponent(this)[0]);
+		String result = "bar " + description;
 		
 //		int index = ((int) this.getBottomRightInDiagram().getXCoordinate());
 //		
