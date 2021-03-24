a5f24af43640f5c63f65afd121d92b741864911f
xpernoca <xhulio.pernoca@gmail.com>
2021-03-24 21:47:21 +0100

    Differ inPin outPin icons

--- Implementation Phase/GELF/src/main/java/gelf/view/composites/MainWindow.java
+++ Implementation Phase/GELF/src/main/java/gelf/view/composites/MainWindow.java
@@ -50,7 +50,7 @@ public class MainWindow extends Window implements Updatable {
     public SubWindowArea subWindowArea;
     // colors/graphics
     Color cBackground = ColorTheme.frame;
-    Image icon = Toolkit.getDefaultToolkit().getImage("src/main/java/gelf/view/composites/Images/AppIcon.png");
+    Image icon = Toolkit.getDefaultToolkit().getImage("src/main/resources/AppIcon.png");
     // other
     String version = "0.0.0";
 
